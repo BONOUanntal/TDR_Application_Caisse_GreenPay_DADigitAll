05ad1082bd035ea30b8e980b673130afb6b64ac5
BONOUanntal <anntal.bonou-zinsou@epitech.eu>
2026-08-06 17:54:32 +0100

    Add: manager can see the proof

--- TDR_Application_Caisse_GreenPay_DADigitAll/src/components/manager/proofs/ProofCard.jsx
+++ TDR_Application_Caisse_GreenPay_DADigitAll/src/components/manager/proofs/ProofCard.jsx
@@ -4,6 +4,9 @@ export default function ProofCard({
     onReject,
 }) {
 
+    const fichierUrl =
+        `http://127.0.0.1:8000/storage/${preuve.chemin_fichier}`;
+
     return (
 
         <div className="border rounded-xl p-5 flex justify-between items-center">
@@ -11,35 +14,22 @@ export default function ProofCard({
             <div>
 
                 <h3 className="font-semibold text-lg">
-
                     {preuve.demande.motif}
-
                 </h3>
 
                 <p className="text-gray-500 mt-1">
-
                     {preuve.demande.user.name}
-
                 </p>
 
                 <p className="text-sm text-gray-400">
-
                     {preuve.demande.entreprise.nom}
-
                 </p>
 
                 <p className="mt-3">
-
                     <span className="font-medium">
-
-                        Montant réel :
-
-                    </span>
-
-                    {" "}
-
-                    {Number(preuve.montant_reel).toLocaleString("fr-FR")} FCFA
-
+                        Montant déclaré :
+                    </span>{" "}
+                    {Number(preuve.montant_declare).toLocaleString("fr-FR")} FCFA
                 </p>
 
             </div>
@@ -47,43 +37,26 @@ export default function ProofCard({
             <div className="flex gap-3">
 
                 <a
-
-                    href={`http://127.0.0.1:8000/storage/${preuve.fichier}`}
-
+                    href={fichierUrl}
                     target="_blank"
-
                     rel="noreferrer"
-
                     className="px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300"
-
                 >
-
                     Voir
-
                 </a>
 
                 <button
-
                     onClick={() => onValidate(preuve.id)}
-
-                    className="px-4 py-2 rounded-lg bg-green-600 text-white hover:bg-green-700"
-
+                    className="px-4 py-2 rounded-lg bg-green-600 text-white"
                 >
-
                     Valider
-
                 </button>
 
                 <button
-
                     onClick={() => onReject(preuve.id)}
-
-                    className="px-4 py-2 rounded-lg bg-red-600 text-white hover:bg-red-700"
-
+                    className="px-4 py-2 rounded-lg bg-red-600 text-white"
                 >
-
                     Rejeter
-
                 </button>
 
             </div>
